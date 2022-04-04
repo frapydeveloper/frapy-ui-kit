d1a13a6600574588f89fc64a077704d672ee817f
frapybalazswelker <balazs.welker@frapy.co>
2022-04-04 18:10:06 +0200

    Card, Typography added to UI Kit

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,9 @@
 import "./style/global.scss";
 
 import Button from "./components/Button";
+import Card from "./components/Card";
 import Input from "./components/Input";
+import Typography from "./components/Typography";
 import Variable from "./components/Variable";
 
-export { Button, Input, Variable };
+export { Button, Card, Input, Typography, Variable };
