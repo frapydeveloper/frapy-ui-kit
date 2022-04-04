61770e2dbb6ca6aff0fc9a8bd0316a2ba914b9c6
frapybalazswelker <balazs.welker@frapy.co>
2022-04-04 12:44:47 +0200

    Input added to the package

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import "./style/global.scss";
 
 import Button from "./components/Button";
+import Input from "./components/Input";
 import Variable from "./components/Variable";
 
-export { Button, Variable };
+export { Button, Input, Variable };
