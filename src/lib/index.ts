a19e3bf9777fbc32f1bb0a13e965259d882b1cd4
frapybalazswelker <balazs.welker@frapy.co>
2022-04-04 18:52:29 +0200

    Logo added to UI Kit

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,7 +3,8 @@ import "./style/global.scss";
 import Button from "./components/Button";
 import Card from "./components/Card";
 import Input from "./components/Input";
+import Logo from "./components/Logo";
 import Typography from "./components/Typography";
 import Variable from "./components/Variable";
 
-export { Button, Card, Input, Typography, Variable };
+export { Button, Card, Input, Logo, Typography, Variable };
