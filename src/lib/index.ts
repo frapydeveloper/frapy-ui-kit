e7c365f82438e364a85ecc88ae6be41a97b14954
frapybalazswelker <balazs.welker@frapy.co>
2022-04-04 23:48:35 +0200

    Divider added to package

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,9 +2,10 @@ import "./style/global.scss";
 
 import Button from "./components/Button";
 import Card from "./components/Card";
+import Divider from "./components/Divider";
 import Input from "./components/Input";
 import Logo from "./components/Logo";
 import Typography from "./components/Typography";
 import Variable from "./components/Variable";
 
-export { Button, Card, Input, Logo, Typography, Variable };
+export { Button, Card, Divider, Input, Logo, Typography, Variable };
