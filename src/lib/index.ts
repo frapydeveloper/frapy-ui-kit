5cbe3bfdab55cfea6287dc6d09387db1b697a9f4
frapybalazswelker <balazs.welker@frapy.co>
2022-04-05 23:28:59 +0200

    Sidenav and navbar added to UI kit

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,7 +5,19 @@ import Card from "./components/Card";
 import Divider from "./components/Divider";
 import Input from "./components/Input";
 import Logo from "./components/Logo";
+import Navbar from "./components/Navbar";
+import Sidenav from "./components/Sidenav";
 import Typography from "./components/Typography";
 import Variable from "./components/Variable";
 
-export { Button, Card, Divider, Input, Logo, Typography, Variable };
+export {
+  Button,
+  Card,
+  Divider,
+  Input,
+  Logo,
+  Navbar,
+  Sidenav,
+  Typography,
+  Variable,
+};
