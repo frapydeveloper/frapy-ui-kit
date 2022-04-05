f41ab9c5433767f82b4068ce2577f2ad063a6622
frapybalazswelker <balazs.welker@frapy.co>
2022-04-05 23:41:19 +0200

    v0.0.12 published

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,6 +7,8 @@ import Input from "./components/Input";
 import Logo from "./components/Logo";
 import Navbar from "./components/Navbar";
 import Sidenav from "./components/Sidenav";
+import SidenavMenu from "./components/SidenavMenu";
+import SidenavMenuItem from "./components/SidenavMenuItem";
 import Typography from "./components/Typography";
 import Variable from "./components/Variable";
 
@@ -18,6 +20,8 @@ export {
   Logo,
   Navbar,
   Sidenav,
+  SidenavMenu,
+  SidenavMenuItem,
   Typography,
   Variable,
 };
