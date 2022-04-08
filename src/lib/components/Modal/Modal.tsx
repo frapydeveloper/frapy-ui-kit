5bcad0e679c2349276ca6fc26d7fee410cd67d06
frapybalazswelker <balazs.welker@frapy.co>
2022-04-08 12:13:03 +0200

    [#70] Modal created

---
+++ src/lib/components/Modal/Modal.tsx
@@ -0,0 +1,65 @@
+import "./modal.scss";
+
+import React, { ReactElement, useRef } from "react";
+import Button from "../Button";
+import { ArrowDropDown } from "@frapy/icons";
+import { onClickOutside } from "@frapy/utils";
+
+interface IRequiredProps {
+  modalHeading: string;
+  onModalClose: Function;
+}
+
+interface IOptionalProps {
+  size?: "sm" | "md" | "lg" | "xlg";
+  children?: JSX.Element | JSX.Element[];
+  primaryButtonText?: string;
+  onSubmitRequest?: Function | undefined;
+}
+
+interface IProps extends IRequiredProps, IOptionalProps {}
+
+const defaultProps: IOptionalProps = {
+  size: "sm",
+};
+
+function Modal({
+  modalHeading,
+  onModalClose,
+  size,
+  children,
+  primaryButtonText,
+  onSubmitRequest,
+}: IProps): ReactElement {
+  const target = useRef(null);
+  onClickOutside({ target, callback: () => onModalClose() });
+
+  return (
+    <div className="modal-container">
+      <div className={`modal-wrapper ${size}`} ref={target}>
+        <div className="modal-header-wrapper">
+          <div className="title">{modalHeading}</div>
+          <Button
+            color="secondary"
+            kind="tertiary"
+            renderIcon={<ArrowDropDown />}
+            hasIconOnly
+            onClick={() => onModalClose()}
+          />
+        </div>
+        <div className="modal-content-wrapper">{children}</div>
+        {primaryButtonText && (
+          <div className="modal-footer-wrapper">
+            <Button onClick={() => onSubmitRequest && onSubmitRequest()}>
+              {primaryButtonText}
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+Modal.defaultProps = defaultProps;
+
+export default Modal;
