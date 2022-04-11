8286db0638883d5ad3ecf0a06945c30a4bd2d814
frapybalazswelker <balazs.welker@frapy.co>
2022-04-11 13:40:44 +0200

    [#66] Avatar created

---
+++ src/lib/components/Avatar/Avatar.tsx
@@ -0,0 +1,45 @@
+import "./avatar.scss";
+
+import React, { ReactElement } from "react";
+
+interface IRequiredProps {
+  name: string;
+}
+
+interface IOptionalProps {
+  type?: "compact" | "full";
+  size?: "sm" | "md" | "lg" | "xlg";
+  avatar?: string;
+  onClick?: Function;
+}
+
+interface IProps extends IRequiredProps, IOptionalProps {}
+
+function Avatar({
+  name,
+  type = "compact",
+  size = "sm",
+  avatar,
+  onClick,
+}: IProps): ReactElement {
+  return (
+    <div
+      className={`avatar-container ${type} ${size}`}
+      onClick={() => onClick && onClick()}
+    >
+      {avatar ? (
+        <div className={`icon-wrapper`}>
+          <img src={avatar} alt="avatar-img" />
+        </div>
+      ) : (
+        <div className={`icon-wrapper`}>
+          {name?.split(" ")[0]?.substring(0, 1)}
+          {name?.split(" ")[1]?.substring(0, 1)}
+        </div>
+      )}
+      {type === "full" && <div className="name-wrapper">{name}</div>}
+    </div>
+  );
+}
+
+export default Avatar;
