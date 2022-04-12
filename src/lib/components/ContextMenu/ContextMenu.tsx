2c51865ed7ca4e0276dade3ac3f0200d01fb355d
frapybalazswelker <balazs.welker@frapy.co>
2022-04-12 14:50:42 +0200

    [#69] ContextMenu created

---
+++ src/lib/components/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,30 @@
+import "./context-menu.scss";
+
+import React from "react";
+
+import useContextMenu from "../../../../../frapy-utils/src/lib/hooks/useContextMenu";
+
+interface IRequiredProps {
+  id: string;
+}
+
+interface IOptionalProps {
+  children?: JSX.Element | JSX.Element[] | string;
+}
+
+interface IContextMenu extends IRequiredProps, IOptionalProps {}
+
+function ContextMenu({ id, children }: IContextMenu) {
+  const { anchorPoint, show } = useContextMenu({ id });
+
+  if (show) {
+    return (
+      <ul className="menu" style={{ top: anchorPoint.y, left: anchorPoint.x }}>
+        {children}
+      </ul>
+    );
+  }
+  return <></>;
+}
+
+export default ContextMenu;
---
+++ src/lib/components/ContextMenuItem/ContextMenuItem.tsx
@@ -0,0 +1,22 @@
+import "./context-menu-item.scss";
+
+import React from "react";
+
+interface IRequiredProps {}
+
+interface IOptionalProps {
+  children?: JSX.Element | JSX.Element[] | string;
+  onClick?: Function;
+}
+
+interface IContextMenuItem extends IRequiredProps, IOptionalProps {}
+
+function ContextMenuItem({ children, onClick }: IContextMenuItem) {
+  return (
+    <li className="context-menu-item" onClick={() => onClick && onClick()}>
+      {children}
+    </li>
+  );
+}
+
+export default ContextMenuItem;
